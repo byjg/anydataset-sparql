fe2cd1e64e7b6f9ce09bd6dc6ae6dd9ef3e20613
Joao Gilberto Magalhaes <joao@byjg.com.br>
2018-11-19 22:15:08 -0600

    Split from Anydataset. Initial Commit.

---
+++ README.md
@@ -0,0 +1,52 @@
+# SparQL AnyDataset
+
+[![Opensource ByJG](https://img.shields.io/badge/opensource-byjg.com-brightgreen.svg)](http://opensource.byjg.com)
+[![Build Status](https://travis-ci.org/byjg/anydataset-sparql.svg?branch=master)](https://travis-ci.org/byjg/anydataset-sparql)
+
+## Description
+
+SparQL abstraction dataset. Anydataset is an agnostic data source abstraction layer in PHP. 
+
+See more about Anydataset [here](https://github.com/byjg/anydataset).
+
+## Examples
+
+### Simple Manipulation
+
+```php
+<?php
+
+$sparqlEndpoint = 'http://dbpedia.org/sparql';
+
+$namespace = [
+    'dbo' => 'http://dbpedia.org/ontology/',
+    'dbp' => 'http://dbpedia.org/property/'
+];
+
+$dataset = new \ByJG\AnyDataset\Semantic\SparQLDataset($sparqlEndpoint, $namespace);
+$iterator = $dataset->getIterator("select distinct ?Concept where {[] a ?Concept} LIMIT 5");
+
+foreach ($iterator as $row) {
+    echo $row->get("Concept");
+    echo $row->get("Concept.type");
+}
+```
+
+## Install
+
+Just type: 
+
+```
+composer require "byjg/anydataset-sparql=4.0.*"
+```
+
+#### Running Unit tests
+
+Running the Unit tests
+
+```php
+vendor/bin/phpunit
+```
+
+----
+[Open source ByJG](http://opensource.byjg.com)
---
+++ src/SparQLIterator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace ByJG\AnyDataset\Semantic;
+
+use ByJG\AnyDataset\Core\GenericIterator;
+use ByJG\AnyDataset\Core\Exception\IteratorException;
+use ByJG\AnyDataset\Core\Row;
+use ByJG\Serializer\Exception\InvalidArgumentException;
+use SparQL\Result;
+
+class SparQLIterator extends GenericIterator
+{
+
+    /**
+     * @var Result
+     */
+    private $sparqlQuery;
+
+    /**
+     * Enter description here...
+     *
+     * @var int
+     */
+    private $current = 0;
+
+    public function __construct(Result $sparqlQuery)
+    {
+        $this->sparqlQuery = $sparqlQuery;
+
+        $this->current = 0;
+    }
+
+    public function count()
+    {
+        return ($this->sparqlQuery->numRows());
+    }
+
+    /**
+     * @access public
+     * @return bool
+     */
+    public function hasNext()
+    {
+        if ($this->current < $this->count()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * @access public
+     * @return Row
+     * @throws IteratorException
+     * @throws InvalidArgumentException
+     */
+    public function moveNext()
+    {
+        if (!$this->hasNext()) {
+            throw new IteratorException("No more records. Did you used hasNext() before moveNext()?");
+        }
+
+        if ($row = $this->sparqlQuery->fetchArray()) {
+            $row = new Row($row);
+            $this->current++;
+            return $row;
+        } else {
+            throw new IteratorException("No more records. Unexpected behavior.");
+        }
+    }
+
+    public function key()
+    {
+        return $this->current;
+    }
+}
